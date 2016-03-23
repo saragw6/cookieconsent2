e4f696f95f711c6705ada5360d79713b2a4fb586
Sara <saragw6@gmail.com>
2016-03-22 21:36:43 -0400

    adding eager app store compatibility

---
+++ eagerapp.js
@@ -0,0 +1,11 @@
+(function(){
+
+  // Set cookieconsent_options using INSTALL_OPTIONS
+  window.cookieconsent_options = INSTALL_OPTIONS;
+
+  // This is used by the preview to enable live updating of the app while previewing.
+  // Uses update_cookieconsent_options function to customize Cookie Consent "on the fly"
+  INSTALL_SCOPE = {
+    setOptions: function (opts) { window.update_cookieconsent_options(opts); }
+  };
+})()
