d95f3fbb08b9ebef64ebe67ce7b3127fded8a2a5
Sara <saragw6@gmail.com>
2016-03-23 21:25:30 -0400

    line break changes

--- eagerapp.js
+++ eagerapp.js
@@ -6,6 +6,8 @@
   // This is used by the preview to enable live updating of the app while previewing.
   // Uses update_cookieconsent_options function to customize Cookie Consent "on the fly"
   INSTALL_SCOPE = {
-    setOptions: function (opts) { window.update_cookieconsent_options(opts); }
+    setOptions: function (opts) {
+    	window.update_cookieconsent_options(opts);
+    }
   };
 })()
